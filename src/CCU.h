94c2ef598913bd8288f4b596b7e1e8fc2a76e3cd
lalbarakat <lalbarakat@gmail.com>
2015-03-27 03:03:47 -0300

    Fix  CCU to include Node

--- src/CCU.h
+++ src/CCU.h
@@ -4,13 +4,13 @@
 #include <vector>
 #include "Job.h"
 #include "Task.h"
-//#include "Node.h"  // By Laith we will use shared memory for communication 
+#include "Node.h"  // By Laith we will use shared memory for communication 
 #include "CCU_Node.h"
 #include "CCU_PJS.h"
 
 class CCU {
 public:
-   // CCU(std::vector <Node*> _node_list); // By Laith we will use shared memory for communication
+    CCU(std::vector <Node*> _node_list); // By Laith we will use shared memory for communication
     CCU(const CCU& orig);
     virtual ~CCU();
     int apply_matrix(Task t);
@@ -19,7 +19,7 @@ public:
 private:
     void init_matrix();
     std::vector<std::vector<int>> wait_time_matrix;
-  //  std::vector <Node*> node_list; // By Laith we will use shared memory for communication
+    std::vector <Node*> node_list; // By Laith we will use shared memory for communication
     
 
 };
