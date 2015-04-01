aff88632a28fd8c9504dd3ce6688d01024a544f0
Matthew <Matthew@idea-PC>
2015-04-01 18:30:03 -0500

    Made PJS_Node.

--- src/PJS_Node.cpp
+++ src/PJS_Node.cpp
@@ -9,3 +9,16 @@ PJS_Node::PJS_Node(const PJS_Node& orig) {
 PJS_Node::~PJS_Node() {
 }
 
+void PJS_Node::addTask(Task t){
+    queue_mutex.lock();
+    task_queue.push(t);
+    queue_mutex.unlock();
+}
+
+Task PJS_Node::getTask(){
+    queue_mutex.lock();
+    Task t= task_queue.front();
+    task_queue.pop();
+    queue_mutex.unlock();
+    return t;
+}
