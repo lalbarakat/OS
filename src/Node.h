f4672d7409c6385d57a9cfa02b9f9c8691df2891
lalbarakat <lalbarakat@gmail.com>
2015-04-02 16:58:18 -0300

    Add Node and CPU

--- src/Node.h
+++ src/Node.h
@@ -15,12 +15,37 @@
 #include <thread>
 #include <chrono>
 #include<mutex>
+#include <queue>
 
 //std::mutex node_mutex;
+class CPU;
 
+class Node {
+public:
+    PJS_Node PJSNode;
+    Node_CCU NodeCCU;
+    std::mutex queue_mutex;
+    Node(int _id);
+    Node(const Node& orig);
+    virtual ~Node();
+    void Scheduler();
+    void CreateExecuters(); 
+    void addTask(Task t);
+    Task getTask();
+   // void SumbitTask();
+    //void PrintQ();
+    int getId(){ return id;}
+private:
+    int id;
+    int CORESNUM = 1;
+    int MAINMEMORY = 8192; //8GB
+    std::queue<Task> queue;
+    std::mutex qmutex;
+    //queue of task
+};
 class CPU {
 public:
-    CPU(int _id);
+    CPU(Node*);
     CPU(const CPU& orig);
     virtual ~CPU();
     bool isBusy(){return status;};
@@ -34,25 +59,6 @@ private:
     bool status = false;
 };
 
-class Node {
-public:
-    Node_PJS NodePJS;
-    Node_CCU NodeCCU;
-    Node(int _id);
-    Node(const Node& orig);
-    virtual ~Node();
-    void Scheduler();
-    void CreateExecuters();    
-    void SumbitTask();
-    void PrintQ();
-    int getId(){ return id;}
-private:
-    int id;
-    int CORESNUM = 1;
-    int MAINMEMORY = 8192; //8GB
-    std::vector<Task> QTasks;
-    //queue of task
-};
 
 #endif	/* NODE_H */
 
