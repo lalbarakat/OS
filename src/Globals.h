6f5d463a4a2d065121815d13ac2225942f3ade3f
Matthew <Matthew@idea-PC>
2015-04-03 12:10:04 -0500

    Added global header file.

---
+++ src/Globals.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Globals.h
+ * Author: Matthew
+ *
+ * Created on April 3, 2015, 12:08 PM
+ */
+
+#ifndef GLOBALS_H
+#define	GLOBALS_H
+#include <mutex>
+std::mutex output_mutex;
+
+#endif	/* GLOBALS_H */
+
