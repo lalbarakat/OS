07081b5a5f3b57e0228702bc0f019e5fcd124d87
Matthew <Matthew@idea-PC>
2015-04-11 12:14:23 -0500

    Test commit

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
     std::cout<<"Main thread still here"<<std::endl;
     output_mutex.unlock();
     CCU ccu_obj(Nodes_list);
-    std::this_thread::sleep_for(std::chrono::seconds(120));
+    std::this_thread::sleep_for(std::chrono::seconds(60));
     return 0;
 }
 
